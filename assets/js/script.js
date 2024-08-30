24c72db97b4bf37b695f3661a04da5d3b874fe9c
Joshua Jay N. Javier <115388721+shintakino@users.noreply.github.com>
2024-08-30 17:59:54 +0800

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    
+    // Typing animation
+    var typed = new Typed(".typing", {
+        strings: ["Data Analyst (Python/SQL/R/Excel/Tableau)", "Frontend Developer (Angular, React)", "Backend Developer (Django, Flask, javascript, Next.js)", "Designer (Figma, Canva, Miro, Adobe Illustrator)"], // , "Freelancer", "Blogger"
+        typeSpeed: 70,
+        backSpeed: 50,
+        loop: true
+    });
+});
